910623cc30efddb434f0ae84194e86c6ef93f409
Ishaan2011 <ishaanchowdhry2011@gmail.com>
2025-05-06 21:19:00 +0530

    Commit 2 for exec file and removal of excess code

--- exec.py
+++ exec.py
@@ -3,16 +3,17 @@ import os
 from libraries import recordLIB, sentiment_analysisLIB,transcribeLIB
 rc = recordLIB.rcn()
 
-if __name__ == "__main__":
+def main():
     input_mp3 = "tempR.mp3"  
 
     try:
         transcription = transcribeLIB.transcribe_audio_with_faster_whisper(input_mp3)
 
-        print(transcription)
-        print("sentiment...")
+        return transcription,sentiment_analysisLIB.analyze_emotion(transcription)
 
-        sentiment_analysisLIB.analyze_emotion(transcription)
     finally:
         if os.path.exists(input_mp3):
             os.remove(input_mp3)
+
+
+main()
